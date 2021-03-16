852f638c3564194aba20afb6941410329a69549b
Parthiban <parthiban192@gmail.com>
2021-03-16 11:31:15 +0530

    March 16

--- DataStructures/Arrays/FindMissingNumber.py
+++ DataStructures/Arrays/FindMissingNumber.py
@@ -2,7 +2,7 @@
 
 print('My solution ')
 
-a = [1, 2, 4,3,6]
+a = [1, 2, 4, 3, 6]
 a.sort()
 c = 1
 for i in a:
@@ -11,14 +11,25 @@ for i in a:
         break
     c += 1
 
-
-print('Summation')
+print('using Summation')
 
 size = len(a)
 n = size + 1
-sum = n*(n+1)/2
+sum = n * (n + 1) / 2
 arr_sum = 0
 for i in a:
     arr_sum += i
 
 print(int(sum - arr_sum))
+
+print('Using XOR')
+
+a = [1, 2, 4, 3, 6]
+n = len(a)
+x1 = 0
+x2 = 1
+for i in a:
+    x1 = x1 ^ i
+for i in range(2, n + 2):
+    x2 = x2 ^ i
+print(x1 ^ x2)
