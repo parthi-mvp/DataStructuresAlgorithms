a4a1b8dca4209e0c70d29376b5557094e5034db0
Parthiban <parthiban192@gmail.com>
2021-03-16 12:11:43 +0530

    March 16

--- DataStructures/Arrays/FindMissingNumber.py
+++ DataStructures/Arrays/FindMissingNumber.py
@@ -1,8 +1,9 @@
 # https://www.geeksforgeeks.org/find-the-missing-number/
 
-print('My solution ')
+print('My solution #1')
 
-a = [1, 2, 4, 3, 6]
+a = [2, 3, 6, 5, 7, 4]
+n = len(a)
 a.sort()
 c = 1
 for i in a:
@@ -10,6 +11,26 @@ for i in a:
         print(c)
         break
     c += 1
+
+print("My solution #2")
+
+
+def findmissing(arr, s, e):
+    for i in range(s, e):
+        if arr[i - 1] != i:
+            print(c)
+            break
+
+
+mid = n // 2
+if a[mid] != mid + 1 and a[mid - 1] == mid and a[mid] == mid + 2:
+    print(mid + 1)
+elif a[mid] != mid + 1:
+
+    findmissing(a[:mid], 1, mid + 1)
+else:
+
+    findmissing(a[mid:], mid, n)
 
 print('using Summation')
 
