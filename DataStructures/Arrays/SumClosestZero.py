bc3c77d7a42c2e1dabe8d57dd101f6f7ee2d8492
Parthiban <parthiban192@gmail.com>
2021-03-17 17:23:03 +0530

    March 17

--- DataStructures/Arrays/SumClosestZero.py
+++ DataStructures/Arrays/SumClosestZero.py
@@ -1,3 +1,30 @@
+# https://www.geeksforgeeks.org/two-elements-whose-sum-is-closest-to-zero/
+
+arr = [1, 60, -10, 70, -80, 85, 2]
+size = len(arr)
+arr.sort()
+l = 0
+r = size - 1
+el1 = arr[l]
+el2 = arr[r]
+min_sum = arr[l] + arr[r]
+
+while l < r:
+    sum = arr[l] + arr[r]
+    if abs(sum) < abs(min_sum):
+        min_sum = sum
+        el1 = arr[l]
+        el2 = arr[r]
+    if sum < 0:
+        l += 1
+    else:
+        r -= 1
+
+print(el1, el2, 'Sum : ', min_sum)
+
+print("Greek soluton - Wrong one ")
+# issue with the sorting - Works if array is sorted
+
 import sys
 
 
@@ -14,8 +41,8 @@ def findMinSum(arr, n):
 
     for i in range(1, n):
 
-        # Absolute value shows how 
-        # close it is to zero 
+        # Absolute value shows how
+        # close it is to zero
         if (abs(arr[i - 1] + arr[i]) <= Min):
             # If found an even close value
             # update min and store the index
@@ -28,7 +55,8 @@ def findMinSum(arr, n):
 
 
 # Driver code
-arr = [2, 1, 60, -10, 70, -80]
+arr = [1, 60, -10, 70, -80, 85, 2]
+
 n = len(arr)
 
 findMinSum(arr, n)
