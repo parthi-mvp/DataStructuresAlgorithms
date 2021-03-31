3f8b07c766fc6f84ad178e239adb4f2b6edf2105
Parthiban <parthiban192@gmail.com>
2021-03-31 11:30:19 +0530

    March 31

--- DataStructures/Arrays/FindPeakElement.py
+++ DataStructures/Arrays/FindPeakElement.py
@@ -1,6 +1,6 @@
 # https://www.geeksforgeeks.org/find-a-peak-in-a-given-array/
 
-arr = [50,50,70]
+arr = [1, 3, 20, 4, 1, 0]
 
 n = len(arr)
 
@@ -13,3 +13,19 @@ if arr[n - 1] > arr[n - 2]:
 for i in range(1, n - 1):
     if arr[i] > arr[i - 1] and arr[i] > arr[i + 1]:
         print(arr[i])
+
+
+# Brinary search approach
+
+def findpeak(arr, low, high,n):
+    mid = low + (high + low)
+
+    if (mid == 0 or arr[mid-1] <= arr[mid]) and (mid == n-1 or arr[mid+1] <= arr[mid]):
+        return mid
+
+    elif mid > 0 and arr[mid-1] > arr[mid]:
+        return  findpeak(arr,low,mid-1,n)
+    else:
+        return findpeak(arr, mid+1, high, n)
+
+print(findpeak(arr,0,n-1,n))
